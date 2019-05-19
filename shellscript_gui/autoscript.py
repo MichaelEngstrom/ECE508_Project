931f6dcfbed6f1ae678c6cef00eb81d237396aa6
Michael Engstrom <highwire32@yahoo.com>
2019-05-19 05:00:34 -0700

    Add, Remove script paths
    Can Add and Remove script paths from list. Update in text window. Keep
    count of tests.

--- shellscript_gui/autoscript.py
+++ shellscript_gui/autoscript.py
@@ -2,8 +2,8 @@
 
     GUI to help manage running shell scripts in Linux
     File dialog to open scripts
-    Scripts saved one per line in file
-    Read each line in file and execute scripts with
+    Script paths saved one per line in list
+    Read each path in list and execute scripts with
     saved console output in file for each
     Nice to Have: count of scripts and remaining or
     number completed
@@ -11,11 +11,20 @@
     Developed using Python 3.5"""
 
 import tkinter as tk
+import subprocess
+import os
+from tkinter import filedialog
+from tkinter import StringVar
+from datetime import datetime as dt
+
+# Create list to hold script paths
+scripts_list = []
 
 class ScriptRunner:
     def __init__(self):
         # Create the main window
         self.main_window = tk.Tk()
+        self.main_window.title("ScriptRunner - Select Scripts to Run Tests")
 
         # Create the frames
         self.button_frame = tk.Frame(self.main_window)
@@ -28,15 +37,19 @@ class ScriptRunner:
         self.addtest_button = tk.Button(self.button_frame, \
                                         text='Add Test', \
                                         command=self.add_test)
+        self.removetest_button = tk.Button(self.button_frame, \
+                                        text='Remove Last Test',\
+                                        command=self.remove_test)
         self.runtest_button = tk.Button(self.button_frame, \
-                                        text='Run test(s)',\
+                                        text='Run Test(s)',\
                                         command=self.run_test)
         self.quit_button = tk.Button(self.button_frame, \
                                      text='Quit', \
                                      command=self.main_window.destroy)
-        self.addtest_button.pack(side=tk.LEFT, pady=10, anchor='w')
-        self.runtest_button.pack(side=tk.LEFT, pady=10)
-        self.quit_button.pack(side=tk.LEFT, pady=10)
+        self.addtest_button.pack(side=tk.LEFT, pady=10, padx=20)
+        self.removetest_button.pack(side=tk.LEFT, pady=10, padx=20)
+        self.runtest_button.pack(side=tk.LEFT, pady=10, padx=20)
+        self.quit_button.pack(side=tk.LEFT, pady=10, padx=20)
 
         # Create and pack widget for test label frame
         self.tests_label = tk.Label(self.testlabel_frame, \
@@ -45,22 +58,24 @@ class ScriptRunner:
 
         # Create and pack widget for text window frame
         self.tests_text = tk.Text(self.textwindow_frame, \
-                                  height=15, width=60)
+                                  height=15, width=100)
         self.tests_text.pack(expand='YES', fill=tk.BOTH, pady=3)
 
         # Create and pack widgets for number of tests frame
-        self.numtests_label = tk.Label(self.numtests_frame, \
-                                       text='#')
         self.teststorun_label = tk.Label(self.numtests_frame, \
-                                         text='Tests to Run')
-        self.numtests_label.pack(side=tk.LEFT)
+                                         text='Tests to Run: ')
+        self.runcount = tk.StringVar()  # To update numtests_label
+        self.numtests_label = tk.Label(self.numtests_frame, \
+                                       textvariable=self.runcount)
         self.teststorun_label.pack(side=tk.LEFT)
+        self.numtests_label.pack(side=tk.LEFT)
 
         # Create and pack widgets for tests completed frame
-        self.numcomp_label = tk.Label(self.comptests_frame, \
-                                      text='#')
         self.testcomp_label = tk.Label(self.comptests_frame, \
-                                       text='Tests Completed')
+                                       text='Tests Completed: ')
+        self.completed = tk.StringVar() # To update numcomp_label
+        self.numcomp_label = tk.Label(self.comptests_frame, \
+                                      textvariable=self.completed)
         self.numcomp_label.pack(side=tk.LEFT)
         self.testcomp_label.pack(side=tk.LEFT)
 
@@ -74,21 +89,50 @@ class ScriptRunner:
         # Start the main loop
         tk.mainloop()
 
-    # The add_test method is the callback function for the
-    # addtest_button widget
-
+    # The add_test method is the callback function for the addtest_button widget
     def add_test(self):
-        #file dialog
-        print('printy!')
+        # Use file dialog to add script path to list
+        files = filedialog.askopenfilenames(filetypes =(("Text File", "*.sh"),
+                                                        ("All Files","*.*")),
+                                            title = "Choose a shell script file.")
+        if files:
+            # Copy path to string and strip characters then append to path list
+            temp = str(files)
+            temp = temp.replace("(", "").replace(")", "")
+            temp = temp.replace("'", "").replace(",", "")
+            scripts_list.append(temp)
 
-    # The run_test method is the callback function for the
-    # runtest_button widget
+            # Write changes to text box
+            self.update_text_box()
 
+
+    # The remove test method pops the last test in the list and writes to text box
+    def remove_test(self):
+        if scripts_list:    #if not empty
+            scripts_list.pop()
+            self.update_text_box()
+
+    # The run_test method is the callback function for the runtest_button widget
     def run_test(self):
         #run the tests
-        print('printoo!')
+        for script in scripts_list:
+            print(script)
+
+    # Method to update text box
+    def update_text_box(self):
+        # Clear text box for writing appended list
+        self.tests_text.delete('1.0', tk.END)
+        
+        # Write list of scripts to text box
+        for path in scripts_list:
+            # Append path to line in text box
+            self.tests_text.insert(tk.END, path)
+            self.tests_text.insert(tk.END, '\n')
+            
+        # Update 'Tests to Run' count in GUI for number of scripts selected
+        self.runcount.set(len(scripts_list))
+    
 
 # Create an instance of the ScriptRunner class
-
 script_run = ScriptRunner()
     
